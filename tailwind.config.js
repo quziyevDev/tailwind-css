bf6bb6a91c58c00e6391c13da9f68779d28f920b
quziyevDev <quziyevyusufjon0@gmail.com>
2023-04-22 15:01:41 +0500

    the-end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/index.html','./src/main.css'
+  ],
   theme: {
-    extend: {},
+    extend: {}, 
   },
   plugins: [],
 }
